dc05dd6a08e26e129de043d06f58ae331b2a46dc
Abdelilah Jabri <abdelilah@tousfacteurs.com>
2025-05-07 00:09:08 +0100

    Add template

--- src/Form/CityType.php
+++ src/Form/CityType.php
@@ -6,6 +6,8 @@ use App\Entity\Category;
 use App\Entity\City;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +16,24 @@ class CityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('imageUrl')
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'Nom de la ville'],
+                'label' => false,
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['placeholder' => 'Description'],
+                'label' => false,
+            ])
+            ->add('imageUrl', TextType::class, [
+                'attr' => ['placeholder' => 'URL de l’image'],
+                'label' => false,
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-'choice_label' => 'id',
-            ])
-        ;
+                'choice_label' => 'name',
+                'placeholder' => 'Sélectionnez une catégorie',
+                'label' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
